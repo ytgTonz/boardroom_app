a99776279ee170eb045cf9e3df21b5356c4c087f
ytgTonz <ytgtonz@gmail.com>
2025-07-29 11:59:09 +0200

    Auto-commit: Update AdminBookings.tsx

--- frontend/src/components/AdminBookings.tsx
+++ frontend/src/components/AdminBookings.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { bookingsAPI } from '../services/api';
 import { Booking } from '../types';
+import ConfirmationModal from './ConfirmationModal';
 
 const AdminBookings: React.FC = () => {
   console.log('AdminBookings component rendering...');
