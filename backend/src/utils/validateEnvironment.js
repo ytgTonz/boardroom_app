9a49a330946d153cbf2f7b0710a005142cdcacc0
ytgTonz <ytgtonz@gmail.com>
2025-08-05 09:27:15 +0200

    Auto-commit: Update validateEnvironment.js

--- backend/src/utils/validateEnvironment.js
+++ backend/src/utils/validateEnvironment.js
@@ -10,6 +10,7 @@ const colors = {
   yellow: (text) => `\x1b[33m${text}\x1b[0m`,
   blue: (text) => `\x1b[34m${text}\x1b[0m`,
   cyan: (text) => `\x1b[36m${text}\x1b[0m`,
+  gray: (text) => `\x1b[90m${text}\x1b[0m`,
   bold: (text) => `\x1b[1m${text}\x1b[0m`
 };
 
